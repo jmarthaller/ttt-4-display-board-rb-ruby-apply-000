d7384887ed156a60c2c7bb10c84e9082b20e373a
Jacob Ross Marthaller <marthaller.jr@gmail.com>
2020-09-26 15:21:24 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,10 @@
 def display_board(board)
   pipe = "|"
   line = "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+ 
+  puts " #{board[0]} #{pipe} #{board[1]} #{pipe} #{board[2]} "
+  puts "#{line}"
+  puts " #{board[3]} #{pipe} #{board[4]} #{pipe} #{board[5]} "
+  puts "#{line}"
+  puts " #{board[6]} #{pipe} #{board[7]} #{pipe} #{board[8]} "
 end
